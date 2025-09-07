a106be3342ad000482904ef1e285777bef1156e4
Andy Jeremy Dove <andy.jeremy.dove@gmail.com>
2025-09-07 21:05:07 +0700

    Chore: Create initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250907_135008 from './20250907_135008';
+
+export const migrations = [
+  {
+    up: migration_20250907_135008.up,
+    down: migration_20250907_135008.down,
+    name: '20250907_135008'
+  },
+];
