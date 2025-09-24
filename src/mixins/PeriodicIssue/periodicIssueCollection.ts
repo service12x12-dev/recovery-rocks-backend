7c23c0c6c2a6772f4ec76a0b3c901a5851438e12
Andy Jeremy Dove <andy.jeremy.dove@gmail.com>
2025-09-25 03:17:53 +0700

    Feature: Move periodic issue fields to the sidebar

--- src/mixins/PeriodicIssue/periodicIssueCollection.ts
+++ src/mixins/PeriodicIssue/periodicIssueCollection.ts
@@ -5,59 +5,52 @@ import afterRead from '@/mixins/PeriodicIssue/afterRead'
 
 const fields = [
   {
-    type: 'row',
+    label: 'Ежегодная дата',
+    name: 'annualDate',
+    type: 'group',
+    interfaceName: 'DateStruct',
+    admin: {
+      position: 'sidebar',
+      description:
+        'Каждый год в эту дату участник будет видеть эту публикацию, если для него нет публикации по достигнутым дням',
+    },
     fields: [
       {
-        label: 'Ежегодная дата',
-        name: 'annualDate',
-        type: 'group',
-        interfaceName: 'DateStruct',
-        admin: {
-          description: 'Каждый год в эту дату участник будет видеть эту публикацию, если для него нет публикации по достигнутым дням',
-        },
-        fields: [
-          {
-            type: 'row',
-            fields: [
-              {
-                label: 'День',
-                name: 'day',
-                type: 'number',
-                min: 1,
-                max: 31,
-                validate: validateDay,
-                index: true,
-              },
-              {
-                label: 'Месяц',
-                name: 'month',
-                type: 'number',
-                min: 0,
-                max: 11,
-                validate: validateMonth,
-                index: true,
-                admin: {
-                  components: {
-                    Cell: '@/components/MonthCell',
-                    Field: '@/components/MonthFieldClient',
-                  },
-                },
-              },
-            ],
-          },
-        ],
+        label: 'День',
+        name: 'day',
+        type: 'number',
+        min: 1,
+        max: 31,
+        validate: validateDay,
+        index: true,
       },
       {
-        label: 'Достигнуто дней',
-        name: 'daysReached',
+        label: 'Месяц',
+        name: 'month',
         type: 'number',
+        min: 0,
+        max: 11,
+        validate: validateMonth,
         index: true,
-        unique: true,
         admin: {
-          description: 'От нуля и выше. В первый день чистоты достигнуто ноль дней.',
+          components: {
+            Cell: '@/components/MonthCell',
+            Field: '@/components/MonthFieldClient',
+          },
         },
       },
     ],
+  },
+  {
+    label: 'Достигнуто дней',
+    name: 'daysReached',
+    type: 'number',
+    index: true,
+    unique: true,
+    admin: {
+      position: 'sidebar',
+      description: 'От нуля и выше. В первый день чистоты достигнуто ноль дней.',
+    },
   },
 ] satisfies Field[]
 
